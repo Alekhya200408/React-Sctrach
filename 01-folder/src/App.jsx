cb10019156e4417412d82730056b13b14cacc507
Alekhya Ghosh <officialalekhya2004@gmail.com>
2026-07-10 13:53:53 +0530

    01 folder added

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,18 @@
+
+const App = () => {
+  return (
+    // <>This is called fragmernts/Wrapers so many elements we can put in the function and pass them</>
+    <>
+    <div id="parent">
+      <h1 id="child1">Hello</h1>
+      <h2 id="child2">Hello2</h2>
+    </div>
+
+    <div id="parent2">
+    <h1 id="child3">Hello3</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
